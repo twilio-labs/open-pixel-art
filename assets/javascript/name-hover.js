75d75b81710f73d5ccf980817f3e5730782e13de
Alex Day <AlexanderDavid@users.noreply.github.com>
2019-10-01 18:14:32 -0700

    feat: added pixel contributor indicator (#70)
    * Added username as a tag to the rect objects in the SVG
    
    * Added text box below canvas that shows the username of the pixel under the mouse
    
    * Reconciled package-lock
    
    * Reconciled package-lock
    
    * Changed variable names to camelCase and fetched upstream
    
    * Ran prettier
    
    * Added method comment
    
    fixes #62

---
+++ assets/javascript/name-hover.js
@@ -0,0 +1,33 @@
+// Wait until the window has loaded to add the event handler
+window.onload = () => {
+  // Add a mouse move event handler to the canvas
+  document.getElementById('canvas').onmousemove = pixelHover;
+};
+
+/**
+ * Print the contributer of a pixel on the canvas
+ * @param  {Event} evt MouseEvent from onmousemove event
+ */
+function pixelHover(evt) {
+  // Get the element under the mouse (or tap)
+  mousedOverEl = document.elementFromPoint(evt.clientX, evt.clientY);
+
+  // Check if the moused over element is a RECT
+  if (mousedOverEl.nodeName == 'rect') {
+    // If it is a rect then get the "name" attribute
+    rectName = mousedOverEl.getAttribute('name');
+    // Check if there is a name associated with that block then set
+    // the contributer name span to the name
+    if (rectName != null) {
+      document.getElementById('contributer-name').textContent = rectName;
+    }
+    // If there is no associated name then set the text to nothing
+    else {
+      document.getElementById('contributer-name').textContent = '';
+    }
+  }
+  // If the element is not a rect set the text to nothing
+  else {
+    document.getElementById('contributer-name').textContent = '';
+  }
+}
