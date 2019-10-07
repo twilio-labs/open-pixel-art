b45ead8964f994d452fc752a6ddfa934b23dcb4b
KevBelisle <kev.belisle@gmail.com>
2019-10-07 13:44:47 -0400

    test(danger): Add tests for allPatchesAreForTheSamePixel (#623)
    * test(danger): Remove useless console logs
    
    * test(danger): Rename variables for clarity
    
    * test(danger): Add tests for allPatchesAreForTheSamePixel
    
    * test(danger): Add missing trailing slash

--- __tests__/dangerfile.test.js
+++ __tests__/dangerfile.test.js
@@ -24,8 +24,119 @@ const {
 } = require('../dangerfile');
 
 describe('allPatchesAreForTheSamePixel', () => {
-  test('placeholder', () => {
-    expect(true).toBeTruthy();
+  test('fails with multiple add operations', () => {
+    const jsonPatch = {
+      diff: [
+        {
+          op: 'add',
+          path: '/data/0/'
+        },
+        {
+          op: 'add',
+          path: '/data/1/'
+        }
+      ]
+    };
+    expect(allPatchesAreForTheSamePixel(jsonPatch)).toBe(false);
+  });
+
+  test('fails if remove operation on claimed pixel', () => {
+    const jsonPatch = {
+      diff: [
+        {
+          op: 'remove',
+          path: '/data/0/'
+        }
+      ],
+      before: {
+        data: [
+          {
+            username: 'not_unclaimed'
+          }
+        ]
+      }
+    };
+    expect(allPatchesAreForTheSamePixel(jsonPatch)).toBe(false);
+  });
+
+  test('fails if replace operation on claimed pixel', () => {
+    const jsonPatch = {
+      diff: [
+        {
+          op: 'remove',
+          path: '/data/0/'
+        }
+      ],
+      before: {
+        data: [
+          {
+            username: 'not_unclaimed'
+          }
+        ]
+      }
+    };
+    expect(allPatchesAreForTheSamePixel(jsonPatch)).toBe(false);
+  });
+
+  test('succeeds if remove operation on unclaimed pixel', () => {
+    const jsonPatch = {
+      diff: [
+        {
+          op: 'remove',
+          path: '/data/0/'
+        }
+      ],
+      before: {
+        data: [
+          {
+            username: '<UNCLAIMED>'
+          }
+        ]
+      }
+    };
+    expect(allPatchesAreForTheSamePixel(jsonPatch)).toBe(true);
+  });
+
+  test('succeeds if replace operation on unclaimed pixel', () => {
+    const jsonPatch = {
+      diff: [
+        {
+          op: 'remove',
+          path: '/data/0/'
+        }
+      ],
+      before: {
+        data: [
+          {
+            username: '<UNCLAIMED>'
+          }
+        ]
+      }
+    };
+    expect(allPatchesAreForTheSamePixel(jsonPatch)).toBe(true);
+  });
+
+  test('fails if multiple pixels affected by diffs', () => {
+    const jsonPatch = {
+      diff: [
+        {
+          op: 'replace',
+          path: '/data/0/'
+        },
+        {
+          op: 'add',
+          path: '/data/1/'
+        }
+      ],
+      before: {
+        data: [
+          {
+            username: 'not_unclaimed'
+          }
+        ]
+      }
+    };
+    expect(allPatchesAreForTheSamePixel(jsonPatch)).toBe(false);
   });
 });
 
