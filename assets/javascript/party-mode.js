905c6e5019faa7149c8ea0a19da9e23a281e3c2e
Ryan Kubik <rkubik@twilio.com>
2019-10-15 13:31:34 -0700

    Create party mode loop button

--- assets/javascript/party-mode.js
+++ assets/javascript/party-mode.js
@@ -26,7 +26,8 @@ function onKeyDown(event) {
     t: twist,
     f: flip,
     v: vert,
-    w: walk
+    w: walk,
+    l: loop
   };
   const f = keyMap[key];
   if (f) {
@@ -35,37 +36,64 @@ function onKeyDown(event) {
   }
 }
 
+const loopTimeout = 500;
+let isLooping = false;
+let latestCommand = null;
 const width = 40;
 const height = 40;
 
 function reset() {
+  latestCommand = null;
   transform(({ xStart, yStart }) => [xStart, yStart]);
 }
 
 function random() {
+  latestCommand = random;
   const r = () => Math.floor(Math.random() * 40) * 10;
   transform(() => [r(), r()]);
 }
 
 function order() {
+  latestCommand = order;
   const f = ({ i }) => [(i % width) * 10, Math.floor(i / width) * 10];
   transform(f);
 }
 
 function flip() {
+  latestCommand = flip;
   transform(({ x, y }) => [390 - x, y]);
 }
 
 function vert() {
+  latestCommand = vert;
   transform(({ x, y }) => [x, 390 - y]);
 }
 
 function twist() {
+  latestCommand = twist;
   transform(({ x, y }) => [390 - y, x]);
 }
 
 function walk() {
+  latestCommand = walk;
   transform(w);
+}
+
+function loop() {
+  isLooping = !isLooping;
+  loopFunction();
+}
+
+function loopFunction() {
+  if (!isLooping) {
+    return;
+  }
+
+  if (latestCommand) {
+    latestCommand();
+  }
+
+  setTimeout(loopFunction, loopTimeout);
 }
 
 const nudges = [[10, 0], [-10, 0], [0, 10], [0, -10]];
