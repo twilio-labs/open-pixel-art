0be8e3fc37bdb8bb779423858a5e4e5ded8a3e23
Marcelo Jasek <marcjal.26@gmail.com>
2019-10-28 15:22:40 -0300

    feat: add preview button show all effects at one (#1220)
    * Adding Preview Button to Layout
    
    * Adding preview function
    
    * Add username to pixels.json
    
    * Change name of preview function

--- assets/javascript/party-mode.js
+++ assets/javascript/party-mode.js
@@ -34,8 +34,10 @@ function onKeyDown(event) {
     f: flip,
     v: vert,
     w: walk,
-    a: toggleImages
+    a: toggleImages,
+    p: runAll
   };
+
   const f = keyMap[key];
   if (f) {
     f();
@@ -45,6 +47,23 @@ function onKeyDown(event) {
 
 const width = 40;
 const height = 40;
+
+function runAll() {
+  setTimeEffect(order, 1000);
+  setTimeEffect(reset, 2000);
+  setTimeEffect(twist, 3000);
+  setTimeEffect(flip, 4000);
+  setTimeEffect(vert, 5000);
+  setTimeEffect(random, 6000);
+  setTimeEffect(walk, 7000);
+  setTimeEffect(reset, 8000);
+}
+
+function setTimeEffect(effect, time) {
+  setTimeout(() => {
+    effect();
+  }, time);
+}
 
 function reset() {
   transform(({ xStart, yStart }) => [xStart, yStart]);
