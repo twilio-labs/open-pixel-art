4fed60cdf62894fd038b38192cb2a376cc0f40e6
PoojaChirp <pshashidhar@twitter.com>
2020-02-29 16:18:10 -0500

    create functions.py code to work with functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+import sys
+
+def hail_friend(firstname):
+    print(f"Hail, {firstname}!")
+    
+def add_numbers(num1,num2):
+    sum = num1+num2
+    print(f"{sum}")
+    return sum
+
+hail_friend('Jonathan Joestar')
+value = add_numbers(3,5)
