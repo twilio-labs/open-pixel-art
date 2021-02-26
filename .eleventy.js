008e34bcee37e21aea20805a1784d3f2a6b89a74
johnkismet <mojomanftw@gmail.com>
2021-02-26 12:57:25 -0500

    Hoping this works

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
-
+console.log('Will this work?');
 // File path on how we know it's a docker instance
 const dockerEnvFile = '/.dockerenv';
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('styles');
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy({
-    'node_modules/nes.css/css': 'assets/nes.css'
+    'node_modules/nes.css/css': 'assets/nes.css',
   });
 
   // This is so we can expose the docker port and host to be accesible
   if (fs.existsSync(dockerEnvFile)) {
     eleventyConfig.setBrowserSyncConfig({
       notify: false,
-      host: '0.0.0.0'
+      host: '0.0.0.0',
     });
   }
 
   return {
-    htmlTemplateEngine: 'njk'
+    htmlTemplateEngine: 'njk',
   };
 };
