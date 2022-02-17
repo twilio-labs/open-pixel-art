d1273cd1f8202a1a32f3031f51327160edfe0fbb
Gustavo Labela <gustavolabela@hotmail.com>
2022-02-17 16:45:09 -0300

    https://github.com/twilio-labs/open-pixel-art/pull/<PULL_REQUEST_NUMBER>
    https://github.com/twilio-labs/open-pixel-art/pull/<PULL_REQUEST_NUMBER>

--- utils/new-pixel-twilio/teste.js
+++ utils/new-pixel-twilio/teste.js
@@ -2,3 +2,4 @@ console.log('Pull Request')
 console.log('Pull Request#')
 
 https://github.com/twilio-labs/open-pixel-art/pull/<8888>
+https://github.com/twilio-labs/open-pixel-art/pull/<PULL_REQUEST_NUMBER>
