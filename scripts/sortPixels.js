2f3a428d5c37099bd63008cfe8d72dd627627245
Gustavo Labela <gustavolabela@hotmail.com>
2022-02-17 16:28:51 -0300

    dffsf

--- scripts/sortPixels.js
+++ scripts/sortPixels.js
@@ -17,3 +17,24 @@ readFile(pixelFilePath, { encoding: 'utf8' })
     writeFile(pixelFilePath, sortedPixelString);
   })
   .catch(console.log);
+
+  const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const { sortPixels, pixelsToString } = require('../utils/pixels-helper');
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+const pixelFilePath = path.join('_data', 'pixels.json');
+
+readFile(pixelFilePath, { encoding: 'utf8' })
+  .then(pixelFileData => {
+    const pixels = JSON.parse(pixelFileData);
+
+    const sortedPixelString = pixelsToString(sortPixels(pixels));
+
+    writeFile(pixelFilePath, sortedPixelString);
+  })
+  .catch(console.log);
+
