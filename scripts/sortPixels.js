ad7d46c16d731841302dbe67fefa496b47eeb511
Sam Elmer <selmer@myleadpoint.com>
2022-05-13 13:55:48 +1000

    remove comment

--- scripts/sortPixels.js
+++ scripts/sortPixels.js
@@ -7,7 +7,7 @@ const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
 const pixelFilePath = path.join('_data', 'pixels.json');
-// Read the json file
+
 readFile(pixelFilePath, { encoding: 'utf8' })
   .then(pixelFileData => {
     const pixels = JSON.parse(pixelFileData);
